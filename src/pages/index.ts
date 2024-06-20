189f9d5f6e7f1980301ea77ca02bddd6166b0379
Jae Min Ko <jvc123456@khu.ac.kr>
2024-06-20 09:06:49 +0900

    chore(index.ts): add export path

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,6 @@
+export * from 'src/pages/ContributionPage';
 export * from 'src/pages/CoursePage';
+export * from 'src/pages/LoginPage';
 export * from 'src/pages/MainPage';
 export * from 'src/pages/MbtiPage';
 export * from 'src/pages/router';
