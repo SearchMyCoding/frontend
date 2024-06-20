47b4355e46ccd5c43ef9ca49f65f972bd11a6d5e
Jae Min Ko <jvc123456@khu.ac.kr>
2024-06-20 09:06:01 +0900

    init(page): init contribution page and login page

---
+++ src/pages/ContributionPage.tsx
@@ -0,0 +1,5 @@
+export function ContributionPage(): JSX.Element {
+  return (
+    <p>기여 페이지입니다.</p>
+  );
+}
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,5 @@
+export function LoginPage(): JSX.Element {
+  return (
+    <p>로그인 페이지입니다.</p>
+  );
+}
