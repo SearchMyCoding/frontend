d4c54c66e10f50d8ef111a9a73904cb311291305
Jae Min Ko <jvc123456@khu.ac.kr>
2024-09-10 23:19:40 +0900

    feat: add mbti teat type

--- src/mbti/type.ts
+++ src/mbti/type.ts
@@ -1,44 +1,23 @@
-export const ATTENTION_FOCUS = {
-  EXTRAVERTED: 'E',
-  INTROVERTED: 'I',
-} as const;
+export const enum ATTENTION_FOCUS {
+  EXTRAVERTED = 'E',
+  INTROVERTED = 'I',
+};
 
-export const PERCEPTION_FUNCTION = {
-  SENSING: 'S',
-  INTUITION: 'N',
-} as const;
+export const enum PERCEPTION_FUNCTION {
+  SENSING = 'S',
+  INTUITION = 'N',
+};
 
-export const JUDGMENT_FUNCTION = {
-  THINKING: 'T',
-  FEELING: 'F',
-} as const;
+export const enum JUDGMENT_FUNCTION {
+  THINKING = 'T',
+  FEELING = 'F',
+};
 
-export const LIFESTYLE = {
-  JUDGING: 'J',
-  PERCEIVING: 'P',
-} as const;
+export const enum LIFESTYLE {
+  JUDGING = 'J',
+  PERCEIVING= 'P',
+};
 
-export const MBTI = {
-  ATTENTION_FOCUS,
-  PERCEPTION_FUNCTION,
-  JUDGMENT_FUNCTION,
-  LIFESTYLE
-} as const;
-
-export type AttentionFocus = (typeof ATTENTION_FOCUS)[keyof typeof ATTENTION_FOCUS];
-export type PerceptionFunction = (typeof PERCEPTION_FUNCTION)[keyof typeof PERCEPTION_FUNCTION];
-export type JudgmentFunction = (typeof JUDGMENT_FUNCTION)[keyof typeof JUDGMENT_FUNCTION];
-export type Lifestyle = (typeof LIFESTYLE)[keyof typeof LIFESTYLE];
-
-export type MbtiSingleType =
-  | AttentionFocus
-  | PerceptionFunction
-  | JudgmentFunction
-  | Lifestyle;
-
-export type MbtiType = `${AttentionFocus}${PerceptionFunction}${JudgmentFunction}${Lifestyle}`;
-
-export type MbtiParentType = keyof typeof MBTI;
-export type MbtiChildType<T extends MbtiParentType> = keyof typeof MBTI[T];
-export type MbtiOppositeType<T extends MbtiParentType, G extends MbtiChildType<T>> = Exclude<MbtiChildType<T>, G>;
-export type MbtiShortType<T extends MbtiChildType<G>, G extends MbtiParentType> = typeof MBTI[G][T];
+export type MbtiSingleType = ATTENTION_FOCUS | PERCEPTION_FUNCTION | JUDGMENT_FUNCTION | LIFESTYLE;
+export type MbtiType = `${ATTENTION_FOCUS}${PERCEPTION_FUNCTION}${JUDGMENT_FUNCTION}${LIFESTYLE}`;
+export type MbtiPairMapping<T extends MbtiSingleType, K extends keyof T> = K extends keyof T ? Exclude<keyof T, K> : never;
